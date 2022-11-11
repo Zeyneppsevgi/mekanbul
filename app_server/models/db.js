2563adcae902d96708c866f5dcb49ee16cfc6049
Zeynep Sevgi <zeyneppsevgi@gmail.com>
2022-11-11 12:13:06 +0300

    mekanbul4

---
+++ app_server/models/db.js
@@ -0,0 +1,28 @@
+var mongoose=require("mongoose");
+// var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://mekan32:z123456@mekanbul.6i0gk4f.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+        mongoose.connection.close(function(){
+                console.log(msg);
+                callback();
+
+        });
+        
+}
+process.on("SIGINI",function(){
+        kapat("Uygulama kapatıldı",function(){
+                process.exit(0);
+        });
+});
+mongoose.connection.on("connected",function(){
+        console.log(dbURI+" adresindeki veritabanına bağlanıldı.");
+});
+mongoose.connection.on("disconnected",function(){
+        console.log("Bağlantı koptu.");
+});
+mongoose.connection.on("error",function(){
+        console.log("Bağlantı hatası.");
+});
+
+require("./mekansema");
